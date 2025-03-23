ac85031c98b947807882b095ce6b2189e7df38aa
gubloise <gustavombloise5@gmail.com>
2025-03-23 16:12:44 -0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,4 @@
+function mudarTema() {
+    document.body.style.background = 
+        document.body.style.background === "black" ? "linear-gradient(135deg, #0f2027, #203a43, #2c5364)" : "black";
+}
